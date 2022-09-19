27c4a5b7f39300bb3af2103a006e76f1762a39dd
CharlesSBL <77samaritan77@proton.me>
2022-09-19 12:08:18 -0400

    build_about

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import _ from 'lodash';
 import "./style.scss";
 import "./JavaScript/another.js";
+
+
+// Navigation bar effects on scroll
+window.addEventListener("scroll", (() => {
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+}))
