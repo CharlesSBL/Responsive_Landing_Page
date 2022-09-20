9bdbcfc8cb67d75349632823786c8bfece8121aa
CharlesSBL <77samaritan77@proton.me>
2022-09-20 17:41:37 -0400

    Build_Services

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
+// ============ Imports ============ \\
 import _ from 'lodash';
 import "./style.scss";
 import "./JavaScript/another.js";
 
 
-// Navigation bar effects on scroll
+
+
+// ============ Navigation bar effects on scroll ============ \\
 window.addEventListener("scroll", (() => {
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
-}))
+}));
+
+
+
+
+
+
+// ============ Services section - Modal ============ \\ 
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnMoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+let modal = function (modalClick) {
+    serviceModals[modalClick].classList.add("active");
+};
+
+learnMoreBtns.forEach((learnMoreBtn, i) => {
+    learnMoreBtn.addEventListener("click", () => {
+        modal(i);
+    });
+});
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+        serviceModals.forEach((modalView) => {
+            modalView.classList.remove("active");
+        });
+    });
+});
