f22e6c8a0f8b87148b2bcca5d6e019a0be629839
CharlesSBL <77samaritan77@proton.me>
2022-09-21 18:53:28 -0400

    Build_Portfolio

--- src/index.js
+++ src/index.js
@@ -39,3 +39,32 @@ modalCloseBtns.forEach((modalCloseBtn) => {
         });
     });
 });
+
+
+
+
+
+
+
+// ============ Portfolio Section - Modal ============ \\ 
+const portfolioModals = document.querySelectorAll(".portfolio-model");
+const imgCards = document.querySelectorAll(".img-card");
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
+
+let portfolioModal = function (modalClick) {
+    portfolioModals[modalClick].classList.add("active");
+};
+
+imgCards.forEach((imgCard, i) => {
+    imgCard.addEventListener("click", () => {
+        portfolioModal(i);
+    });
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+    portfolioCloseBtn.addEventListener("click", () => {
+        portfolioModals.forEach((portfolioModalView) => {
+            portfolioModalView.classList.remove("active");
+        });
+    });
+});
