e868b9953a329c3c8693ffc18f03d3299d12a5b1
CharlesSBL <77samaritan77@proton.me>
2022-09-24 12:10:26 -0400

    Build_Dark/Light_Theme

--- src/index.js
+++ src/index.js
@@ -75,7 +75,26 @@ var swiper = new Swiper(".client-swiper", {
 
 
 // ============ Website Dark/Light Theme ============ \\
+const themeBtn = document.querySelector(".theme-btn");
 
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+    themeBtn.classList.toggle("sun");
+
+    localStorage.setItem("saved-theme", getCurrentTheme());
+    localStorage.setItem("saved-icon", getCurrentIcon());
+});
+
+const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light";
+const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
+
+const savedTheme = localStorage.getItem("saved-theme");
+const savedIcon = localStorage.getItem("saved-icon");
+
+if (savedTheme) {
+    document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
+    themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
+};
 
 // ============ Scroll to top Button ============ \\ 
 const scrollTopBtn = document.querySelector(".scrollToTop-btn");
